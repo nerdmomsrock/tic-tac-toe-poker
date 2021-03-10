aa890f4af2fc4b863c9124a6db037a14f40f6bfe
Ryan Morgan <nerdmomsrock@gmail.com>
2021-03-10 11:53:42 -0500

    poker work

--- Part2/index.js
+++ Part2/index.js
@@ -1,1 +1,15 @@
 console.log("The house always wins!")
+
+
+const idInput = document.getElementById('idInput');
+const colorInput = document.getElementById('colorInput');
+
+console.log(idInput);
+console.log(colorInput);
+
+function setCard () {
+    const card = document.getElementById(idInput.value);
+    console.log(card);
+    card.style.color = colorInput.value;
+}
+
